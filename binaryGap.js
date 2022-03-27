8b4226721d97e4d43a9de0fe6ca87f3263edc92d
emmakuen <emmaku@protonmail.com>
2022-03-27 15:23:08 +0900

    update solution

--- binaryGap.js
+++ binaryGap.js
@@ -1,32 +1,21 @@
 function binaryGap(N) {
-  let maxGapLength = 0;
   let gapLength = 0;
-  let binaryList = [];
-  let left = undefined;
-  let right = undefined;
+  let maxGapLength = 0;
+  const binariesString = (N >>> 0).toString(2);
 
-  while (N > 0) {
-    const binary = N % 2;
-    N = Math.floor(N / 2);
-    binaryList.push(binary);
-    const currentIdx = binaryList.length - 1;
-    const prevIdx = binaryList.length - 2;
-    if (!left && !right && binary === 1) {
-      left = currentIdx;
-    } else if (left && !right && binary === 1) {
-      right = currentIdx;
-      gapLength = right - left - 1;
-      maxGapLength = Math.max(gapLength, maxGapLength);
-      left = right;
-      right = undefined;
-    } else if (binaryList[prevIdx] === 1 && binary === 1) {
-      left = currentIdx;
-      right = undefined;
-    } else if (left && right && binary === 0) {
-      left = right;
-      right = undefined;
+  for (let i = 1; i < binariesString.length; i++) {
+    const current = binariesString.charAt(i);
+    const previous = binariesString.charAt(i - 1);
+
+    if (
+      (previous == 1 && current == 0) ||
+      (gapLength > 0 && previous == 0 && current == 1)
+    ) {
+      maxGapLength = Math.max(maxGapLength, gapLength);
+      gapLength = 1;
+    } else if (gapLength > 0 && previous == 0 && current == 0) {
+      gapLength++;
     }
   }
-
   return maxGapLength;
 }
