ae9992ee8736000865c2cd1f8dc7c51cfcc9df98
Emma <85257928+emmakuen@users.noreply.github.com>
2022-04-20 09:20:05 +0900

    add solution of fish problem

---
+++ fish.js
@@ -0,0 +1,35 @@
+// ASSUMPTIONS
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [0..1,000,000,000];
+// each element of array B is an integer that can have one of the following values: 0, 1;
+// the elements of A are all distinct.
+
+function solution(A, B) {
+  if (A.length === 1) return 1;
+
+  let survivors = 0;
+  const stack = [];
+
+  for (let i = 0; i < A.length; i++) {
+    const weight = A[i];
+    if (B[i] === 1) {
+      stack.push(weight);
+    } else {
+      let weightDown = stack.length === 0 ? null : stack.pop();
+      while (weightDown && weightDown < weight) {
+        weightDown = stack.length === 0 ? null : stack.pop();
+      }
+
+      if (!weightDown) {
+        survivors++;
+      } else {
+        stack.push(weightDown);
+      }
+    }
+  }
+
+  survivors += stack.length;
+  return survivors;
+}
+
+console.log(solution([4, 3, 2, 1, 5], [0, 1, 0, 0, 0]));
