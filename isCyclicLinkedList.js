c0887807e5b0fecd17cedede2bbbe0cf75b32c77
Emma <85257928+emmakuen@users.noreply.github.com>
2022-06-21 10:36:54 +0900

    add solution of isCyclicLinkedList

---
+++ isCyclicLinkedList.js
@@ -0,0 +1,23 @@
+// Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const hasCycle = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
