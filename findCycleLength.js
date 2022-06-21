e4353d3106dd4499c84169500a7ed4827252ee33
Emma <85257928+emmakuen@users.noreply.github.com>
2022-06-21 10:57:35 +0900

    remove console.log

--- findCycleLength.js
+++ findCycleLength.js
@@ -30,15 +30,3 @@ const findCycleLength = (head) => {
 
   return cycleLength;
 };
-
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-head.next.next.next.next.next = new Node(6);
-head.next.next.next.next.next.next = head.next.next;
-console.log(`LinkedList cycle length: ${findCycleLength(head)}`);
-
-head.next.next.next.next.next.next = head.next.next.next;
-console.log(`LinkedList cycle length: ${findCycleLength(head)}`);
