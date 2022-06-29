9aa3f084458acfe77e6f833dd56f09a39dc11f8c
Emma <85257928+emmakuen@users.noreply.github.com>
2022-06-29 13:51:40 +0900

    add solution of canAttendAllAppointments

---
+++ canAttendAllAppointments.js
@@ -0,0 +1,37 @@
+// Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+// --- Examples
+// Appointments: [[1,4], [2,5], [7,9]]
+// Output: false
+// Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+// Appointments: [[6,7], [2,4], [8,12]]
+// Output: true
+// Explanation: None of the appointments overlap, therefore a person can attend all of them.
+// Appointments: [[4,5], [2,3], [3,6]]
+// Output: false
+// Explanation: Since [4,5] and [3,6] overlap, a person cannot attend both of these appointments.
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+
+const canAttendAllAppointments = (intervals) => {
+  intervals.sort((a, b) => a.start - b.start);
+
+  for (let i = 1; i < intervals.length; i++) {
+    const previousInterval = intervals[i - 1];
+    const currentInterval = intervals[i];
+
+    if (previousInterval.end > currentInterval.start) {
+      return false;
+    }
+  }
+
+  return true;
+};
